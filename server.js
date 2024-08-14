e8d40393798971bfab8c4128124c3ea9108561e9
BenjaGreen012 <91562169+BenjaGreen012@users.noreply.github.com>
2024-08-14 19:21:38 +0200

    Letra minuscula

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import loginRouter from "./rutas/loginRouter.js";
 import registerRouter from "./rutas/registerRouter.js";
 import habitosRouter from "./rutas/habitosRouter.js";
 import estadisticasRouter from "./rutas/estadisticasRouter.js";
-import SeguimientoHabitosRouter from "./rutas/seguimientoHabitos.js";
+import seguimientoHabitosRouter from "./rutas/seguimientoHabitosRouter.js";
 import cors from "cors";
 
 //importamos express y controladores
@@ -19,7 +19,8 @@ app.use(express.json());
 app.use("/api/login", loginRouter);
 app.use("/api/register", registerRouter);
 app.use("/api/habitos", habitosRouter);
-app.use("/api/stadisticas", estadisticasRouter);
+app.use("/api/estadisticas", estadisticasRouter);
+app.use("/api/seguimientoHabitos", seguimientoHabitosRouter);
 
 
 //arranque del servidor
